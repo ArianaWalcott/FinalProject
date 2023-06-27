9bff99601605573095d9d5a6045b624ec6b44d7b
Ariana <arianawalcott1517@gmail.com>
2023-06-27 14:31:09 -0400

    Created the files for our project

---
+++ FinalProject/SignUp.swift
@@ -0,0 +1,20 @@
+//
+//  SignUp.swift
+//  FinalProject
+//
+//  Created by Antonio Walcott on 6/27/23.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
+}
---
+++ FinalProject/Volunteer.swift
@@ -0,0 +1,20 @@
+//
+//  Volunteer.swift
+//  FinalProject
+//
+//  Created by Antonio Walcott on 6/27/23.
+//
+
+import SwiftUI
+
+struct Volunteer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Volunteer_Previews: PreviewProvider {
+    static var previews: some View {
+        Volunteer()
+    }
+}
