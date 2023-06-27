66bd1c60bc3d3d0277ad41b7eadb52e7ebb20ac7
Ariana <arianawalcott1517@gmail.com>
2023-06-27 09:48:41 -0400

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Antonio Walcott on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
