72ba14f6efdba6d8bf8cbe8ce54f8637e70ff4ad
Nikita <Mandalika>
2023-06-28 14:06:19 -0400

    added the sign up page

--- FinalProject/SignUp.swift
+++ FinalProject/SignUp.swift
@@ -8,12 +8,59 @@
 import SwiftUI
 
 struct SignUp: View {
+    @State private var openText = ""
+    @State private var openText1 = ""
+    @State private var openNum = ""
+    @State private var TextTitle = "Email Address"
+    @State private var TextTitle1 = "First and Last Name"
+    @State private var Age = "What is your age?"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct SignUp_Previews: PreviewProvider {
+        VStack{
+                 Text(TextTitle)
+                     .font(.title)
+                     .bold()
+                    
+                 
+                 TextField("Type Here", text: $openText)
+                     .font(.title2)
+                     .multilineTextAlignment(.center)
+                    .padding()
+                          .background(Color(.systemGroupedBackground))
+                          .cornerRadius(10)
+                         .padding()
+                 
+                 Text(TextTitle1)
+                     .font(.title)
+                     .bold()
+                   
+                 
+                 TextField("Type Here", text: $openText1)
+                     .font(.title2)
+                     .multilineTextAlignment(.center)
+                     
+                    .padding()
+                         .background(Color(.systemGroupedBackground))
+                         .cornerRadius(10)
+                        .padding()
+                 Text(Age)
+                     .font(.title)
+                     .bold()
+                  
+            TextField("Type Here", text: $openNum)
+                .font(.title2)
+                .multilineTextAlignment(.center)
+                .padding()
+                     .background(Color(.systemGroupedBackground))
+                     .cornerRadius(10)
+                    .padding()
+                
+                 
+                     
+                }
+         }
+         }
+         
+        struct SignUp_Previews: PreviewProvider {
     static var previews: some View {
         SignUp()
     }
