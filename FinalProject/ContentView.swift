1f92056450ae70d6cea85d4f5a31157dd92a8508
Elif Akkaya <eakkaya24@my.uaa.k12.tr>
2023-06-29 20:24:42 +0300

    added homepage

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -5,17 +5,44 @@
 //  Created by Antonio Walcott on 6/27/23.
 //
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Ariana Walcott")
+    
+    import SwiftUI
+    
+    struct ContentView: View {
+        var body: some View {
+            NavigationStack {
+            VStack {
+                Image("logo")
+                    .imageScale(.small)
+                    .foregroundColor(.accentColor)
+                
+                
+                NavigationStack {
+                    VStack {
+                        NavigationLink (destination: Text("...")) {
+                            Text("Sign in")
+                                .font(.title3)
+                                .foregroundColor(.white)
+                                .bold()
+                                .padding()
+                                .background(Color.teal, in: RoundedRectangle(cornerRadius: 20))
+                        }
+                        
+                        NavigationLink (destination: Text("...")) {
+                            Text("Sign up")
+                                .font(.title3)
+                                .foregroundColor(.white)
+                                .bold()
+                                .padding()
+                                .background(Color.teal, in: RoundedRectangle(cornerRadius: 20))
+                        }
+                    }
+                }
+                
+                .padding()
+                
+            }
         }
-        .padding()
     }
 }
 
